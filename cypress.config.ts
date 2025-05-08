233e6cf8a5ca05bf18a9639e7ea4bcc32627eb79
Laissa Pereira <laissa.pereira25@gmail.com>
2025-05-08 11:42:41 +0200

    fix config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,32 +1,10 @@
 import { defineConfig } from "cypress";
-import { spawn } from 'child_process';
-import path from 'path';
 
 export default defineConfig({
-    e2e: {
-      baseUrl: 'http://localhost:3000',
-      setupNodeEvents(on, config) {
-        let serverProcess: ReturnType<typeof spawn>;
-  
-        on('before:run', () => {
-          console.log('🔧 Starting http-server...');
-          const serveDir = path.resolve(__dirname, '..', 'task');
-
-          serverProcess = spawn('npx', ['http-server', '.', '-p', '3000'], {
-            cwd: serveDir,
-            shell: true,
-            stdio: 'inherit',
-          });
-        });
-  
-        on('after:run', () => {
-          console.log('🛑 Stopping http-server...');
-          if (serverProcess) {
-            serverProcess.kill();
-          }
-        });
-  
-        return config;
-      },
+  e2e: {
+    baseUrl: 'http://127.0.0.1:3000',
+    setupNodeEvents(on, config) {
+      return config; // No server logic here
     },
-  });
+  },
+});
